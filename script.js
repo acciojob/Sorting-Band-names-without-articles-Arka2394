0041cf858c871434739304aa9d21a5a96ced65b3
Arka2394 <87031423+Arka2394@users.noreply.github.com>
2023-04-19 12:30:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-// Get the ul element
-const ulElement = document.querySelector('ul#bands');
 
-// Define the array of band names
-let bandNames = ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones'];
 
-// Sort the band names in lexicographic order excluding articles
-bandNames.sort(function(a, b) {
-  const firstWordA = a.replace(/^(a |an |the )/i, '').trim();
-  const firstWordB = b.replace(/^(a |an |the )/i, '').trim();
-  return firstWordA.localeCompare(firstWordB);
-});
+    // JavaScript code here
+    // Get the ul element
+    const ulElement = document.querySelector('ul');
 
-// Append the sorted band names to the ul element as list items
-bandNames.forEach(bandName => {
-  const liElement = document.createElement('li');
-  liElement.textContent = bandName;
-  ulElement.appendChild(liElement);
-});
+    // Define the array of band names
+    let bandNames = ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones'];
+
+    // Sort the band names in lexicographic order excluding articles
+    bandNames.sort(function(a, b) {
+      const firstWordA = a.replace(/^(a |an |the )/i, '').trim();
+      const firstWordB = b.replace(/^(a |an |the )/i, '').trim();
+      return firstWordA.localeCompare(firstWordB);
+    });
+
+    // Append the sorted band names to the ul element as list items
+    bandNames.forEach(bandName => {
+      const liElement = document.createElement('li');
+      liElement.textContent = bandName;
+      ulElement.appendChild(liElement);
+    });
